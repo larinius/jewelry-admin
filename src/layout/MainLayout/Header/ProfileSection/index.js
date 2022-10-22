6bf69ac6fb7cbed72e6b91dab0ab8d296d955447
dmitry grunt <dmitry.grunt@protonmail.com>
2022-10-23 02:48:55 +0300

    DND mode switch in profile - removed

--- src/layout/MainLayout/Header/ProfileSection/index.js
+++ src/layout/MainLayout/Header/ProfileSection/index.js
@@ -190,43 +190,7 @@ const ProfileSection = () => {
                                                     bgcolor: theme.palette.primary.light,
                                                     my: 2
                                                 }}
-                                            >
-                                                <CardContent>
-                                                    <Grid container spacing={3} direction="column">
-                                                        <Grid item>
-                                                            <Grid item container alignItems="center" justifyContent="space-between">
-                                                                <Grid item>
-                                                                    <Typography variant="subtitle1">Start DND Mode</Typography>
-                                                                </Grid>
-                                                                <Grid item>
-                                                                    <Switch
-                                                                        color="primary"
-                                                                        checked={sdm}
-                                                                        onChange={(e) => setSdm(e.target.checked)}
-                                                                        name="sdm"
-                                                                        size="small"
-                                                                    />
-                                                                </Grid>
-                                                            </Grid>
-                                                        </Grid>
-                                                        <Grid item>
-                                                            <Grid item container alignItems="center" justifyContent="space-between">
-                                                                <Grid item>
-                                                                    <Typography variant="subtitle1">Allow Notifications</Typography>
-                                                                </Grid>
-                                                                <Grid item>
-                                                                    <Switch
-                                                                        checked={notification}
-                                                                        onChange={(e) => setNotification(e.target.checked)}
-                                                                        name="sdm"
-                                                                        size="small"
-                                                                    />
-                                                                </Grid>
-                                                            </Grid>
-                                                        </Grid>
-                                                    </Grid>
-                                                </CardContent>
-                                            </Card>
+                                            ></Card>
                                             <Divider />
                                             <List
                                                 component="nav"
