32978ff35f55a61b56b2887139d8f8d071ee5a2b
dmitry grunt <dmitry.grunt@protonmail.com>
2022-11-03 04:39:48 +0200

    fix ip

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ const App = () => {
     const customization = useSelector((state) => state.customization);
 
     return (
-        <Uploady destination={{ url: "http://localhost:8080/api/priceupload" }}>
+        <Uploady destination={{ url: "http://139.59.132.107:8080/api/priceupload" }}>
         <QueryClientProvider client={queryClient}>
         <StyledEngineProvider injectFirst>
             <ThemeProvider theme={themes(customization)}>
